7fcfdc14eaf30dfaf7a4cabb88449191d8b690ba
abdala9512 <miguel.arquez12@gmail.com>
2020-10-30 13:18:15 -0500

    Generation forecast endpoint

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+import flask
+from flask import Flask, jsonify
 
+import numpy as np
+from DeepLearningUTSModel import DeepLearningUTSModel
+from tensorflow.keras.models import load_model
+
+LSTM_gen = load_model('output/LSTM_energy_gen.h5')
+LSTM_cap = load_model('output/LSTM_energy_cap.h5')
 
 app = Flask(__name__)
 
@@ -7,9 +14,19 @@ app = Flask(__name__)
 def index():
     return '<h1>LSTM model </h1>'
 
-@app.route('/generation-forecast')
+@app.route('/generation-forecast', methods=['GET', 'POST'])
 def g_forecast():
-    return '<h1>LSTM Energy generation forecast </h1>'
+
+    data = {"success": False}
+    params = flask.request.json
+
+    
+    ts = np.array(params.get("ts"))
+    X, y = DeepLearningUTSModel.create_ts_sequences(series=ts, lag=2)
+    data["response"] = LSTM_gen.predict(X).tolist()
+    data["success"] = True
+
+    return flask.jsonify(data)
 
 @app.route('/capacity-forecast')
 def c_forecast():
@@ -17,4 +34,4 @@ def c_forecast():
 
 
 if __name__ == '__main__':
-    app.run(port='6600')
+    app.run(port='6600', host='0.0.0.0')
